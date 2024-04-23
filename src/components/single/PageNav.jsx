ab374ea1b6b4995a3f9c8e7437d7ec5880d5a917
Muhammad Hendawi <muhendawi@gmail.com>
2024-04-23 12:55:12 +0200

    adding the hamburger button for mobile layout

--- src/components/single/PageNav.jsx
+++ src/components/single/PageNav.jsx
@@ -2,8 +2,11 @@ import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
 import Button from "./Button";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 function PageNav() {
+  const [isMenuOn, setMenu] = useState(false);
   return (
     <>
       <nav className={styles.nav}>
@@ -20,7 +23,33 @@ function PageNav() {
           </li>
         </ul>
         <Button type="black-btn">Get an invite</Button>
+        <RxHamburgerMenu
+          onClick={() => setMenu(!isMenuOn)}
+          className={styles.hamburgerBtn}
+          size={35}
+        />
       </nav>
+      <div
+        className={`${styles.menuContainer} ${
+          isMenuOn ? styles.menuContainerOn : styles.menuContainerOff
+        }`}
+      >
+        <div className={styles.innerMenu}>
+          <ul>
+            <li>
+              <NavLink to="/stories">Stories</NavLink>
+            </li>
+            <li>
+              <NavLink to="/features">Features</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pricing">Pricing</NavLink>
+            </li>
+          </ul>
+
+          <Button type="black-btn">Get an invite</Button>
+        </div>
+      </div>
     </>
   );
 }
